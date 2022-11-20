9ba236bd785fc6059e4a4ce4543a579fef974b5a
Maksym Puzanov <puzanovmaksym@gmail.com>
2022-11-20 16:36:48 +0200

    bug fix font colot to the  burger menu

--- src/js/modules/burgerMenu.js
+++ src/js/modules/burgerMenu.js
@@ -1,8 +1,12 @@
 const iconBurger = document.querySelector('.burger__icon');
 const menuBurger = document.querySelector('.burger-menu');
+const header = document.querySelector('.header');
 
-iconBurger.addEventListener('click', event => {
-	iconBurger.classList.toggle('_active');
-	menuBurger.classList.toggle('_active');
-	document.body.classList.toggle('_lock');
-})
+if (iconBurger) {
+	iconBurger.addEventListener('click', event => {
+		iconBurger.classList.toggle('_active');
+		menuBurger.classList.toggle('_active');
+		header.classList.toggle('_active');
+		document.body.classList.toggle('_lock');
+	})
+}
